47cb722bbd3c1396923ccf39e3f5024773b195a7
Salma WADOUACHI <wadouachisalma@gmail.com>
2024-10-15 13:40:46 +0200

    Initial Commit

---
+++ SwiftUiProject/SwiftUiProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiProjectApp.swift
+//  SwiftUiProject
+//
+//  Created by WADOUACHI on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUiProjectTests/SwiftUiProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiProjectTests.swift
+//  SwiftUiProjectTests
+//
+//  Created by WADOUACHI on 15/10/2024.
+//
+
+import Testing
+@testable import SwiftUiProject
+
+struct SwiftUiProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
